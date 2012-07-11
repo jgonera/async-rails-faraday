3e54ef0f34007dea830014f7dd6d64dd8251aa77
Juliusz Gonera <juliusz_g@iwonka.med.virginia.edu>
2012-07-11 13:17:46 -0400

    Initial commit
    Barebone Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsyncRailsFaraday::Application.config.secret_token = 'b3b52315fb7cc72583394225e354f005f14516e44184f5753a327bed4bdf6de803381beb6c2521ea1d76e833a7f265ea50786a3e02c55487770cc9aac6c216a8'
